28bb5f385e1edc5bc6e6c1ecc489d9a5e14d7578
dalex <delphindidier0@gmail.com>
2021-05-14 00:47:17 -0400

    file update

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const I_URL = "https://api.filtered.ai/q/get-partner-availability";
 return fetch(I_URL)
 .then(data=>{return data.json()})
 .then(res=>{console.log(res)})
-};
+}; 
+
+    
+;
 return getPartners; 
 
